a95d5bb7384f97f36a2e9942965a19380b2e69c7
Daniel Ellis <danielshawellis@gmail.com>
2020-03-22 16:28:02 -0500

    add plugin header and basic security

---
+++ ammonite-custom-popup.php
@@ -0,0 +1,16 @@
+
+<?php
+/**
+ * Plugin Name:       Ammonite Custom Popup
+ * Description:       Adds a custom popup with a newsletter signup form.
+ * Version:           1.0.0
+ * Author:            Daniel Ellis
+ * Author URI:        https://danielellisdevelopment.com/
+ */
+
+ /*
+  Basic Security
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+  die;
+}
