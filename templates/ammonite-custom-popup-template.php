53bdf1cd56a40698deed0cf43fcf3510bc15c3b3
Daniel Ellis <danielshawellis@gmail.com>
2020-03-24 11:26:57 -0500

    resolve Infusionsoft connection issues by replacing form

--- templates/ammonite-custom-popup-template.php
+++ templates/ammonite-custom-popup-template.php
@@ -7,10 +7,10 @@
       <div class="ammonite-custom-popup-right-section-text">
         <?php echo wp_get_attachment_image( 2099, 'medium', false, '' ) ?>
       </div>
-      <form accept-charset="UTF-8" action="https://ay767.infusionsoft.com/app/form/process/cbbd14525742739291f9d8f0f02d4bbc" class="ammonite-custom-popup-right-section-form infusion-form" id="inf_form_cbbd14525742739291f9d8f0f02d4bbc" method="POST">
-          <input name="inf_form_xid" type="hidden" value="cbbd14525742739291f9d8f0f02d4bbc" />
-          <input name="inf_form_name" type="hidden" value="Web Form submitted" />
-          <input name="infusionsoft_version" type="hidden" value="1.70.0.194774" />
+      <form accept-charset="UTF-8" action="https://ay767.infusionsoft.com/app/form/process/5feba70834176f90616665a95b39d1d1" class="ammonite-custom-popup-right-section-form infusion-form" id="inf_form_5feba70834176f90616665a95b39d1d1" method="POST">
+          <input name="inf_form_xid" type="hidden" value="5feba70834176f90616665a95b39d1d1" />
+          <input name="inf_form_name" type="hidden" value="Email Subscription Form Submitted On Creating Health" />
+          <input name="infusionsoft_version" type="hidden" value="1.70.0.206035" />
           <div class="infusion-field">
               <label for="inf_field_FirstName">First Name *</label>
               <input class="infusion-field-input" id="inf_field_FirstName" name="inf_field_FirstName" placeholder="First Name *" type="text" />
@@ -23,15 +23,15 @@
               <label for="inf_field_Email">Email *</label>
               <input class="infusion-field-input" id="inf_field_Email" name="inf_field_Email" placeholder="Email *" type="text" />
           </div>
-          <div class="infusion-submit">
-              <button class="ammonite-custom-popup-button-submit infusion-recaptcha" id="recaptcha_cbbd14525742739291f9d8f0f02d4bbc" type="submit">Submit</button>
+          <div class="ammonite-custom-popup-button-submit infusion-submit">
+              <button class="infusion-recaptcha" id="recaptcha_5feba70834176f90616665a95b39d1d1" type="submit">Submit</button>
               <button class="ammonite-custom-popup-button-exit" type="button">No Thanks</button>
           </div>
       </form>
       <script type="text/javascript" src="https://ay767.infusionsoft.app/app/webTracking/getTrackingCode"></script>
-      <script type="text/javascript" src="https://ay767.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.194774"></script>
+      <script type="text/javascript" src="https://ay767.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.206035-hf-202003151330"></script>
       <script src="https://www.google.com/recaptcha/api.js?onload=onloadInfusionRecaptchaCallback&render=explicit" async="async" defer="defer"></script>
-      <script type="text/javascript" src="https://ay767.infusionsoft.com/app/timezone/timezoneInputJs?xid=cbbd14525742739291f9d8f0f02d4bbc"></script>
+      <script type="text/javascript" src="https://ay767.infusionsoft.com/app/timezone/timezoneInputJs?xid=5feba70834176f90616665a95b39d1d1"></script>
       <div class="">
         <?php echo wp_get_attachment_image( 21, 'medium', false, '' ) ?>
       </div>
